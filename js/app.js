0509e1d8382ab8e7fc03eb35b3b82c4134c1a4c4
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2014-12-26 09:29:14 +0100

    Give more UI

--- js/app.js
+++ js/app.js
@@ -9,16 +9,60 @@
 
     this.dropdown = document.getElementById('actions-items');
     this.details = document.getElementById('action-details');
+    this.controls = document.getElementById('action-controls');
+    this.feedback = document.getElementById('action-feedback');
+
+    this.bind();
+
+    this.actionStarted = false;
   }
 
   ShakeItManager.prototype = {
 
-    toggleDetails: function(visible) {
-      if (!this.details) {
+    bind: function() {
+      if (!this.controls) {
         return;
       }
 
-      this.details.dataset.visible = visible;
+      var start = this.controls.querySelector('#btn-start');
+      var stop = this.controls.querySelector('#btn-stop');
+
+      start.addEventListener('click', this.handleStart.bind(this));
+      stop.addEventListener('click', this.handleStop.bind(this));
+
+      this.updateStartStop();
+    },
+
+    toggleGeneric: function(target, visible) {
+      if (!target) {
+        return;
+      }
+
+      target.dataset.visible = visible;
+    },
+
+    toggleDetails: function(visible) {
+      this.toggleGeneric(this.details, visible);
+    },
+
+    toggleControls: function(visible) {
+      this.toggleGeneric(this.controls, visible);
+    },
+
+    toggleFeedback: function(visible) {
+      this.toggleGeneric(this.feedback, visible);
+    },
+
+    showDetails: function() {
+      this.toggleDetails(true);
+      this.toggleControls(true);
+      this.toggleFeedback(true);
+    },
+
+    hideDetails: function() {
+      this.toggleDetails(false);
+      this.toggleControls(false);
+      this.toggleFeedback(false);
     },
 
     setDetails: function(title, content) {
@@ -36,14 +80,6 @@
 
       this.showDetails();
     },
-
-    showDetails: function() {
-      this.toggleDetails(true);
-    },
-
-    hideDetails: function() {
-      this.toggleDetails(false);
-    },
     
     addAction: function(id, name, title, desc, handler) {
       if (!(id in this.actions)) {
@@ -55,6 +91,35 @@
         };
         this.updateUI(id, name);
       }
+    },
+
+    updateStartStop: function() {
+      var start = this.controls.querySelector('#btn-start');
+      var stop = this.controls.querySelector('#btn-stop');
+
+      if (!this.actionStarted) {
+        start.setAttribute('enabled', 'enabled');
+        start.removeAttribute('disabled');
+        stop.setAttribute('disabled', 'disabled');
+        stop.removeAttribute('enabled');
+      } else {
+        start.setAttribute('disabled', 'disabled');
+        start.removeAttribute('enabled');
+        stop.setAttribute('enabled', 'enabled');
+        stop.removeAttribute('disabled');
+      }
+    },
+
+    handleStart: function(evt) {
+      console.debug('Start');
+      this.actionStarted = true;
+      this.updateStartStop();
+    },
+
+    handleStop: function(evt) {
+      console.debug('Stop');
+      this.actionStarted = false;
+      this.updateStartStop();
     },
 
     handleClick: function(evt) {
